c033b4123bb1b0f88d60dfeb6a12123e0acafe90
Lebedev Timofei <91500245+LebedevTimofey@users.noreply.github.com>
2022-02-27 16:07:10 +0300

    Add files via upload

---
+++ Хоара и Шелла/Шелла и Хоара.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoi(int n, int first, int second, int third)
+{
+    if (n > 0)
+    {
+        hanoi(n - 1, first, third, second);
+        cout << first << " => " << second << endl;
+        hanoi(n - 1, third, second, first);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Кол-во дисков: ";
+    cin >> n;
+    hanoi(n, 1, 3, 2);
+    return 0;
+}
