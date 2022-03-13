dee83e8bc944c78801e4b0ab7f7c3633cef3ec20
Lebedev Timofei <91500245+LebedevTimofey@users.noreply.github.com>
2022-03-13 18:45:00 +0300

    Add files via upload

---
+++ Лаба 11.3/Лаба 11.3 с библиотекой stack.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+
+	int n,el;
+	setlocale(LC_ALL, "ru");
+	stack<int>steck1;
+	cout << "Введите размер стека"<<"\n";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << ">>";
+		cin >> el;
+		if (el % 2 != 0)
+		{
+            steck1.push(el);
+		}
+	}
+	cout << "_____________________" << endl;
+	while (!steck1.empty())
+	{
+		cout << ">>" << steck1.top() << "\n";
+		steck1.pop();
+	}
+}
