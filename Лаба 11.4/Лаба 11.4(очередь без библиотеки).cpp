2aa5d74901caee8f5f65c8814d72ef704b117dd8
Lebedev Timofei <91500245+LebedevTimofey@users.noreply.github.com>
2022-03-13 18:45:18 +0300

    Add files via upload

---
+++ Лаба 11.4/Лаба 11.4(очередь без библиотеки).cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	char data;
+	Node* next;
+	Node* pred;
+};
+
+Node* head;
+
+void AddList(int f, int position)
+{
+	Node* node = new Node;
+	cout << "\n>>";
+	cin >> node->data;
+	if (head == NULL)
+	{
+		node->next = node;
+		node->pred = node;
+		head = node;
+	}
+	else
+	{
+		Node* p = head;
+		if (f == 1)
+		{
+			for (int i = position; i > 1; i--)
+				p = p->next;
+		}
+		p->pred->next = node;
+		node->pred = p->pred;
+		node->next = p;
+		p->pred = node;
+	}
+}
+
+void PrintList()
+{
+	Node* a = head;
+	cout << "\nЭлементы очереди: \n";
+	do
+	{
+		cout << a->data << " ";
+		a = a->next;
+	} while (a != head);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << "Введите размер очереди: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		AddList(0, 0);
+	}
+	PrintList();
+	int k = 0;
+	cout << "На какое место поставить новый элемент?" << "\n";
+	cin >> k;
+	cout << endl;
+	AddList(1, k);
+	PrintList();
+	return 0;
+}
---
+++ Лаба 11.4/Лаба 11.4(очередь с библиотекой).cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	queue<char>q;
+	int n,k,count;
+	count = 0;
+	setlocale(LC_ALL, "ru");
+	char el,extra;
+	cout << "Введите размер очереди:";
+	cin >> n;
+	cout << endl;
+	cout << "Введите новый элемент :";
+	cin >> extra;
+	cout << "Введите его номер:";
+	cin >> k;
+	for (int i = 0; i <= n; i++)
+	{
+		cout << ">>";
+		count += 1;
+		cin >> el;
+		if (k == count)
+		{
+			q.push(extra);
+			q.push(el);
+		}
+		else
+		{
+			q.push(el);
+		}
+	}
+	cout << "________________" << endl;
+	while (!q.empty())
+	{
+		cout <<">>"<< q.front() << endl;
+		q.pop();
+	}
+}
